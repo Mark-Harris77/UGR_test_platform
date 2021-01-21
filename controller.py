322117d5b55d07f8db8be98b0addd97876e28a67
Mark Harris <mark_harris77@hotmail.com>
2021-01-21 22:07:48 +0000

    removed buggy dictionary for an if statement stack

--- controller.py
+++ controller.py
@@ -3,9 +3,14 @@ from car_model import *
 car = car()
 commands {'forward':car.goForward, 'back':car.goBack, 'left':car.goLeft, 'right':car.goRight}
 
-
 while(True):
     command = input("Enter Command: ")
-    if command in commands:
-        commands[command]()
-        car.updateVehicle()
+    if command == 'forward':
+        car.goForward()
+    elif command == 'back':
+        car.goBack()
+    elif command == 'left':
+        car.goLeft()
+    elif command == 'right':
+        car.goRight()
+    car.updateVehicle()
