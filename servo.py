9ef9cdf574cfb3b825e01100ab935abe8b4d95ff
Mark Harris <mark_harris77@hotmail.com>
2021-01-31 19:53:03 +0000

    Added servo controller code

---
+++ servo.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+
+def initalize():
+    GPIO.setmode(GPIO.BCM)
+    servo = GPIO.PWM(7, 50)
+    servo.start(0)
+    return servo
+
+def finish(servo):
+    servo.stop()
+    GPIO.cleanup()
+
+def setWheelAngle(servo, angle):
+    if (angle > 180) or (angle < 0):
+        raise ValueError("Angle value must be between 0 & 180 degrees")
+    servo.ChangeDutyCycle(2+(angle/18))
+    time.sleep(0.5)
+    servo.ChangeDutyCycle(0)
+
+s = initalize()
+
+
+for i in range(10):
+    a = input("Enter angle: ")
+    setWheelAngle(s, a)
+
+finish()
